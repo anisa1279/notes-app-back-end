60956726570e6a1dd5ef4672eb4bcb16c5b96e6f
Anisa Hikmatuni <anisa.hikmatuni@gmail.com>
2023-03-25 18:54:51 +0100

    edit handler

--- src/handler.js
+++ src/handler.js
@@ -104,7 +104,7 @@ const deleteNoteByIdHandler = (request, h) => {
   const index = notes.findIndex((note) => note.id === id);
 
   if (index !== -1) {
-    notes.splices(index, 1);
+    notes.splice(index, 1);
     const response = h.response({
       status: 'success',
       message: 'Catatan berhasil dihapus',
